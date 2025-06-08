8e6d0898c7a4dd61dd033641d459c5cc41b69aca
rayanebendadouch <benadouchrayane@gmail.com>
2025-06-08 12:11:12 +0100

    add mongodb connection

--- cabinetmed/cabinetmed/settings.py
+++ cabinetmed/cabinetmed/settings.py
@@ -78,8 +78,9 @@ WSGI_APPLICATION = 'cabinetmed.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'HOST': 'localhost',
+        'PORT': 27017,
     }
 }
 
